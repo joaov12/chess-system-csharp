9ed3d54452f17859699f62d85ff29ecb5d28a8eb
joaov12 <jvfs12th@gmail.com>
2023-11-09 14:22:35 -0300

    Criando a classe PosicaoXadrez!

---
+++ xadrez/PosicaoXadrez.cs
@@ -0,0 +1,21 @@
+﻿using XadrezUDM.tabuleiro;
+
+namespace XadrezUDM.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
